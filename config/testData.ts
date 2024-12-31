7fc14614d4404aac06a481f2e508ba7adcb5c7e7
Fady <fadytadrous.ft@gmail.com>
2024-12-31 13:40:54 +0200

    added test file and test loop

---
+++ config/testData.ts
@@ -0,0 +1,48 @@
+export const TestData = [
+    // {
+    //     expected: "General Case SLA",
+    //     sharedCaseCat: "general",
+    //     subCategories: [
+    //         "Public Kids Area",
+    //         "Work Permit–Internal Modifications",
+    //         "Work Permit–Swimming Pool/Structure element",
+    //         "Work Permit–Landscape",
+    //         "Work Permit–External Modifications",
+    //         "Work Permit–Furniture",
+    //         "Community Rules-Construction Vehicles",
+    //         "HK Service-Public",
+    //         "Pest Control Service",
+    //         "Delegation Letters",
+    //         "Rental Data Update",
+    //         "Work Permit–Renewal",
+    //         "Client Data Update"
+    //     ]
+    // }, 
+    // {
+    //     expected: "General Case SLA",
+    //     sharedCaseCat: "Facilities tickets", 
+    //     subCategories: [
+    //         "Pest control",
+    //         "Housekeeping",
+    //         "Pets Control",
+    //         "Insurance cheque refund inspection",
+    //         "Triple play letter approval",
+    //         "Gas letter approval",
+    //         "Snag Inspection",
+    //         "DLP Inspection",
+    //         "Water & Electricity supply",
+    //         "General Maintenance ( plumbing and electricity)",
+    //         "Violation"
+    //     ]
+    // },
+    {
+        expected: "General Case SLA",
+        sharedCaseCat: "Landscape",
+        subCategories: [
+            "Irrigation system quotation",
+            "Planting quotation",
+            "Landscape maintenance",
+            "Landscape Public Maintenance"
+        ]
+    }
+];
--- tests/CommunityCareCase.spec.ts
+++ tests/CommunityCareCase.spec.ts
@@ -3,6 +3,7 @@ const { urls } = require('../config/urls.ts');
 import { navBarSelectors } from "../Pages/common/commonSelectors.ts";
 import CasePage from "../Pages/community care/case-page.ts";
 import { expect } from "@playwright/test";
+import { TestData } from "../config/testData.ts";
 
 let casePage: CasePage;
 
@@ -12,39 +13,42 @@ test.beforeEach( async({page}) => {
     casePage = new CasePage(page);
 })
 
-test.describe('Validate that user can create a new case', () => {
+test.describe('Validate that SLA is working for community care', () => {
 
-    // Test case for creating a general community care case
-    test('Create general case', async ({ page }) => { 
-        // Set test to run in slow mode for stability
-        test.slow(); 
-        // make it as a method to be called in the test
-        // Wait for search icon to be visible (ensures page is loaded)
-        await navBarSelectors.searchIcon(page).waitFor();
+    TestData.forEach(({ expected, subCategories,sharedCaseCat }) => {
+        subCategories.forEach((subCategories) => {
+            test(` case category with "${subCategories}" should expect "${expected}"`, async ({ page }) => {
+                // Set test to run in slow mode for stability
+                test.slow(); 
+                
+                // make it as a method to be called in the test
+                // Wait for search icon to be visible (ensures page is loaded)
+                await navBarSelectors.searchIcon(page).waitFor();
 
-        // Fill in the Summary tab information
-        await casePage.summaryTab.click();
-        await casePage.selectItemFromDropdown(casePage.requestType, 1);
-        await casePage.selectItemFromLookup(casePage.caseCategory, 'general');
-        await casePage.selectItemFromLookup(casePage.caseSubCategory, 'Public Kids Area');
-        await casePage.fillItem(casePage.description, 'test description');
+                // Fill in the Summary tab information
+                await casePage.summaryTab.click();
+                await casePage.selectItemFromDropdown(casePage.requestType, 1);
+                await casePage.selectItemFromLookup(casePage.caseCategory, sharedCaseCat);
+                await casePage.selectItemFromLookup(casePage.caseSubCategory, subCategories);
+                await casePage.fillItem(casePage.description, 'test description');
 
-        // Select unit and customer details
-        await casePage.selectItemFromLookup(casePage.unitId, '128');
-        await casePage.selectItemFromLookup(casePage.customer, 'afaf badr');
-        
-        // Save the case
-        await casePage.saveBtn.click();
+                // Select unit and customer details
+                await casePage.selectItemFromLookup(casePage.unitId, '128');
+                await casePage.selectItemFromLookup(casePage.customer, 'afaf badr');
+                
+                // Save the case
+                await casePage.saveBtn.click();
 
-       
-        // Navigate to SLA tab
-        await casePage.slaTab.click();
+            
+                // Navigate to SLA tab
+                await casePage.slaTab.click();
 
-        // Wait for a specific element in the SLA tab to be visible
-        await casePage.slaSection.waitFor();
-        
-        // Check if the SLA item is displayed
-        await expect(casePage.slaItem).toContainText('General Case SLA');
-
+                // Wait for a specific element in the SLA tab to be visible
+                await casePage.slaSection.waitFor();
+                
+                // Check if the SLA item is displayed
+                await expect(casePage.slaItem).toContainText(expected);
+            });
+        });
     });
 });
